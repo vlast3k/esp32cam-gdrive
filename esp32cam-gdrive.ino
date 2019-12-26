f7dc3d651feb39552a3610811139dc389c633641
Vladi Vladi <vlast3k@gmail.com>
2019-12-26 09:10:11 +0200

    add support for larger images and some reordering

--- esp32cam-gdrive.ino
+++ esp32cam-gdrive.ino
@@ -6,13 +6,10 @@
 
 #include "esp_camera.h"
 
-const char* ssid     = "SSID";   //your network SSID
-const char* password = "PASSWORD";   //your network password
+const char* ssid     = "vladiVivacom4g";   //your network SSID
+const char* password = "0888414447";   //your network password
 const char* myDomain = "script.google.com";
-String myScript = "/macros/s/XXXXXXXXXXXXXXXXXXXXXX/exec";    //Replace with your own url
-String myFilename = "filename=ESP32-CAM.jpg";
-String mimeType = "&mimetype=image/jpeg";
-String myImage = "&data=";
+String myScript = "/macros/s/AKfycbyawOlBvc9I-XmiTRZZvA6ZnhROP3LRAjx1jOcrd1xw_iRB7cU/exec";    //Replace with your own url
 
 int waitingTime = 30000; //Wait 30 seconds to google response.
 
@@ -34,13 +31,83 @@ int waitingTime = 30000; //Wait 30 seconds to google response.
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-void setup()
-{
+void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   
   Serial.begin(115200);
   delay(10);
   
+  initWifi();
+  initCamera();
+}
+
+void loop() {
+  saveCapturedImage();
+  delay(60000);
+}
+
+void saveCapturedImage() {
+  Serial.println("Connect to " + String(myDomain));
+  WiFiClientSecure client;
+  
+  if (client.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+    
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();  
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      delay(1000);
+      ESP.restart();
+      return;
+    }
+  
+    char *input = (char *)fb->buf;
+    int encLen = base64_enc_len(fb->len);
+    int chunkSize = 999;
+    char output[base64_enc_len(chunkSize) + 1];
+    Serial.printf("Will convert %ld to %ld bytes\n", fb->len, encLen);
+    Serial.println("Send a captured image to Google Drive.");
+    
+    client.println("POST " + myScript + " HTTP/1.1");
+    client.println("Host: " + String(myDomain));
+    client.println("Content-Length: " + String(encLen));
+    client.println("Content-Type: application/octet-stream");
+    client.println();
+    
+    unsigned long start = millis();
+    for (size_t i=0; i < fb->len;) {
+      size_t x = min((size_t)chunkSize, fb->len - i);
+      base64_encode(output, input + i, x);
+      client.print(output);
+      i+=x;
+    }
+    
+    Serial.printf("Image sent in %ld ms \n", millis() - start);
+    esp_camera_fb_return(fb);
+    
+    Serial.println("Waiting for response.");
+    start = millis();
+    while (!client.available()) {
+      Serial.print(".");
+      delay(100);
+      if ((start + waitingTime) < millis()) {
+        Serial.println("\nNo response.");
+        //If you have no response, maybe need a greater value of waitingTime
+        break;
+      }
+    }
+    Serial.printf("Response Received in %ld ms \n\n", millis() - start);
+    while (client.available()) {
+      Serial.print(char(client.read()));
+    }  
+  } else {         
+    Serial.println("Connected to " + String(myDomain) + " failed.");
+  }
+  client.stop();
+}
+
+void initWifi() {
   WiFi.mode(WIFI_STA);
 
   Serial.println("");
@@ -59,6 +126,9 @@ void setup()
     
   Serial.println("");
 
+}
+
+void initCamera() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -80,7 +150,7 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_VGA;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  config.frame_size = FRAMESIZE_UXGA;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
   config.jpeg_quality = 10;
   config.fb_count = 1;
   
@@ -89,113 +159,5 @@ void setup()
     Serial.printf("Camera init failed with error 0x%x", err);
     delay(1000);
     ESP.restart();
-  }
-}
-
-boolean enviar = true;
-
-void loop() {
-  //if(enviar) {
-    saveCapturedImage();
-    enviar = false;
-    delay(60000);
-  //}
-}
-
-void saveCapturedImage() {
-  Serial.println("Connect to " + String(myDomain));
-  WiFiClientSecure client;
-  
-  if (client.connect(myDomain, 443)) {
-    Serial.println("Connection successful");
-    
-    camera_fb_t * fb = NULL;
-    fb = esp_camera_fb_get();  
-    if(!fb) {
-      Serial.println("Camera capture failed");
-      delay(1000);
-      ESP.restart();
-      return;
-    }
-  
-    char *input = (char *)fb->buf;
-    char output[base64_enc_len(3)];
-    String imageFile = "";
-    for (int i=0;i<fb->len;i++) {
-      base64_encode(output, (input++), 3);
-      if (i%3==0) imageFile += urlencode(String(output));
-    }
-    String Data = myFilename+mimeType+myImage;
-    
-    esp_camera_fb_return(fb);
-    
-    Serial.println("Send a captured image to Google Drive.");
-    
-    client.println("POST " + myScript + " HTTP/1.1");
-    client.println("Host: " + String(myDomain));
-    client.println("Content-Length: " + String(Data.length()+imageFile.length()));
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println();
-    
-    client.print(Data);
-    int Index;
-    for (Index = 0; Index < imageFile.length(); Index = Index+1000) {
-      client.print(imageFile.substring(Index, Index+1000));
-    }
-    
-    Serial.println("Waiting for response.");
-    long int StartTime=millis();
-    while (!client.available()) {
-      Serial.print(".");
-      delay(100);
-      if ((StartTime+waitingTime) < millis()) {
-        Serial.println();
-        Serial.println("No response.");
-        //If you have no response, maybe need a greater value of waitingTime
-        break;
-      }
-    }
-    Serial.println();   
-    while (client.available()) {
-      Serial.print(char(client.read()));
-    }  
-  } else {         
-    Serial.println("Connected to " + String(myDomain) + " failed.");
-  }
-  client.stop();
-}
-
-//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
-String urlencode(String str)
-{
-    String encodedString="";
-    char c;
-    char code0;
-    char code1;
-    char code2;
-    for (int i =0; i < str.length(); i++){
-      c=str.charAt(i);
-      if (c == ' '){
-        encodedString+= '+';
-      } else if (isalnum(c)){
-        encodedString+=c;
-      } else{
-        code1=(c & 0xf)+'0';
-        if ((c & 0xf) >9){
-            code1=(c & 0xf) - 10 + 'A';
-        }
-        c=(c>>4)&0xf;
-        code0=c+'0';
-        if (c > 9){
-            code0=c - 10 + 'A';
-        }
-        code2='\0';
-        encodedString+='%';
-        encodedString+=code0;
-        encodedString+=code1;
-        //encodedString+=code2;
-      }
-      yield();
-    }
-    return encodedString;
+  }  
 }
